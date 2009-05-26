96e00ec6615de8fd3cf5b5c92384b9b355e8a61a
Ranga <crangaranga@gmail.com>
2009-05-26 10:04:01 +0530

    problems in datastructures

---
+++ datastructures.py
@@ -0,0 +1,14 @@
+"""solutions to problems in chapter 2: data structures
+"""
+
+def sum(list):
+    """compute the sum of all the values in a list.
+
+    >>> sum([1, 2, 3])
+    6
+    """
+    pass
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
