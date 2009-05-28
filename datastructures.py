797acc303be1dc0c1dc379d1574bd3026ec4243b
Ranga <crangaranga@gmail.com>
2009-05-28 15:32:46 +0530

    to attach extsort

--- datastructures.py
+++ datastructures.py
@@ -168,6 +168,13 @@ def invertdict(l):
     """
     return dict([(v,k) for k,v in l.items()])
 
+def extsort(list):
+    """a function extsort to sort a list of files based on extension.
+    >>> extsort(['a.c', 'a.py', 'b.py', 'bar.txt', 'foo.txt', 'x.c'])
+    ['a.c', 'x.c', 'a.py', 'b.py', 'bar.txt', 'foo.txt']
+    """
+    return sorted(list, cmp=lambda x, y: cmp(x.split(".")[1], y.split(".")[1    ]))
+
 
 if __name__ == "__main__":       
     import doctest
