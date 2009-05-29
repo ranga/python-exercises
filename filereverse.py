c037bee8f15438067fe9229db64fe113d1f27c41
Ranga <crangaranga@gmail.com>
2009-05-29 16:21:00 +0530

    tab space changed

--- filereverse.py
+++ filereverse.py
@@ -1,34 +1,34 @@
 def filereverse(infile,outfile):
-	"""
-	>>> a program reverse.py to print lines of a file in reverse order.
-        >>>
+    """
+    >>> a program reverse.py to print lines of a file in reverse order.
+    >>>
 
-        $ cat she.txt
-        She sells seashells on the seashore;
-        The shells that she sells are seashells I'm sure.
-        So if she sells seashells on the seashore,
-        I'm sure that the shells are seashore shells.
+    $ cat she.txt
+    She sells seashells on the seashore;
+    The shells that she sells are seashells I'm sure.
+    So if she sells seashells on the seashore,
+    I'm sure that the shells are seashore shells.
 
-        $ python reverse.py she.txt hello.txt
-        $ cat hello.txt
-        I'm sure that the shells are seashore shells.
-        So if she sells seashells on the seashore,
-        The shells that she sells are seashells I'm sure.
-        She sells seashells on the seashore;
+    $ python reverse.py she.txt hello.txt
+    $ cat hello.txt
+    I'm sure that the shells are seashore shells.
+    So if she sells seashells on the seashore,
+    The shells that she sells are seashells I'm sure.
+    She sells seashells on the seashore;
 
-	"""
-	f1=open(outfile ,"w")
-	f2=open(infile ,'r')
-	lines=f2.readlines()
-	for line in lines[::-1]:
-		f1.write(line)
-	f1.close()
-	f2.close()
+    """
+    f1=open(outfile ,"w")
+    f2=open(infile ,'r')
+    lines=f2.readlines()
+    for line in lines[::-1]:
+	f1.write(line)
+    f1.close()
+    f2.close()
 if __name__ == "__main__":
-	import sys
+    import sys
 		
-	infile =sys.argv[1]
-	outfile=sys.argv[2]
-	filereverse(infile, outfile)
+    infile =sys.argv[1]
+    outfile=sys.argv[2]
+    filereverse(infile, outfile)
 
 
--- grep.py
+++ grep.py
@@ -6,6 +6,7 @@ def grep(word, filename):
     So if she sells seashells on the seashore,
     The shells that she sells are seashells I'm sure.
     She sells seashells on the seashore;
+    
     $ python grep.py sure sht.txt
     The shells that she sells are seashells I'm sure.
     I'm sure that the shells are seashore shells.
